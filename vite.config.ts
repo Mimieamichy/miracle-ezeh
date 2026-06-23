82ff79ff2f26263d08e2f42742aa715cc9f9cd41
Mimieamichy <amichy07@gmail.com>
2026-06-23 16:06:21 +0100

    change hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  nitro: {
-    preset: "vercel",
-  }
-})
+  plugins: [
+    nitro({
+      preset: "vercel",
+    }),
+  ],
+});
